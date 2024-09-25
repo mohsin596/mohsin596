0f3d5f5ace88df85910b16e1b75a5787cf07a682
mohsin596 <mohsinalvi.mangotech@gmail.com>
2024-09-25 13:22:20 +0500

    Working on dashboard buttons and Submittenantsform page

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 const DashboardPage = () => {
   const [clientData, setClientData] = useState([]);
@@ -9,23 +9,26 @@ const DashboardPage = () => {
   const [showTenantData, setShowTenantData] = useState(false);
   const [loadingClients, setLoadingClients] = useState(true);
   const [loadingTenants, setLoadingTenants] = useState(true);
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
 
   const handleLogout = () => {
-    localStorage.removeItem('token');
-    window.location.href = '/';
+    localStorage.removeItem("token");
+    window.location.href = "/";
   };
 
   useEffect(() => {
     const fetchClientData = async () => {
       try {
-        const token = localStorage.getItem('token');
-        const response = await axios.get('https://survey.mangotech-api.com/GetDataClientSurvey', {
-          headers: { 'Authorization': `Bearer ${token}` },
-        });
+        const token = localStorage.getItem("token");
+        const response = await axios.get(
+          "https://survey.mangotech-api.com/GetDataClientSurvey",
+          {
+            headers: { Authorization: `Bearer ${token}` },
+          }
+        );
         setClientData(response.data.clientSurvey);
       } catch (err) {
-        setError('Failed to fetch client data');
+        setError("Failed to fetch client data");
         console.error(err);
       } finally {
         setLoadingClients(false);
@@ -34,13 +37,16 @@ const DashboardPage = () => {
 
     const fetchTenantData = async () => {
       try {
-        const token = localStorage.getItem('token');
-        const response = await axios.get('https://survey.mangotech-api.com/GetDataTenantSurvey', {
-          headers: { 'Authorization': `Bearer ${token}` },
-        });
+        const token = localStorage.getItem("token");
+        const response = await axios.get(
+          "https://survey.mangotech-api.com/GetDataTenantSurvey",
+          {
+            headers: { Authorization: `Bearer ${token}` },
+          }
+        );
         setTenantData(response.data.tenantSurvey);
       } catch (err) {
-        setError('Failed to fetch tenant data');
+        setError("Failed to fetch tenant data");
         console.error(err);
       } finally {
         setLoadingTenants(false);
@@ -56,15 +62,29 @@ const DashboardPage = () => {
       <h1 className="text-3xl font-bold mb-6">Dashboard</h1>
 
       {error && <div className="text-red-600">{error}</div>}
-
       <div className="flex space-x-4 mb-4">
-        <button onClick={() => setShowClientData(!showClientData)} className="bg-blue-600 text-white py-2 px-4 rounded-lg hover:bg-blue-700">
+        <button
+          onClick={() => {
+            setShowClientData(!showClientData);
+            setShowTenantData(false); // Hide tenant data when client data is shown
+          }}
+          className="bg-blue-600 text-white py-2 px-4 rounded-lg hover:bg-blue-700"
+        >
           Client Survey
         </button>
-        <button onClick={() => setShowTenantData(!showTenantData)} className="bg-green-600 text-white py-2 px-4 rounded-lg hover:bg-green-700">
+        <button
+          onClick={() => {
+            setShowTenantData(!showTenantData);
+            setShowClientData(false); // Hide client data when tenant data is shown
+          }}
+          className="bg-green-600 text-white py-2 px-4 rounded-lg hover:bg-green-700"
+        >
           Tenants
         </button>
-        <button onClick={handleLogout} className="bg-red-600 text-white py-2 px-4 rounded-lg hover:bg-red-700">
+        <button
+          onClick={handleLogout}
+          className="bg-red-600 text-white py-2 px-4 rounded-lg hover:bg-red-700"
+        >
           Logout
         </button>
       </div>
@@ -72,7 +92,9 @@ const DashboardPage = () => {
       {/* Client Data Table */}
       {showClientData && !loadingClients && clientData.length > 0 && (
         <div className="overflow-x-auto w-full max-w-4xl bg-white rounded-lg shadow-lg p-6">
-          <h2 className="text-xl font-semibold mb-4">Client Survey Responses</h2>
+          <h2 className="text-xl font-semibold mb-4">
+            Client Survey Responses
+          </h2>
           <table className="min-w-full bg-white">
             <thead>
               <tr className="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
@@ -84,12 +106,18 @@ const DashboardPage = () => {
             </thead>
             <tbody className="text-gray-600 text-sm font-light">
               {clientData.map((client) => (
-                <tr key={client._id} className="border-b border-gray-200 hover:bg-gray-100">
+                <tr
+                  key={client._id}
+                  className="border-b border-gray-200 hover:bg-gray-100"
+                >
                   <td className="py-3 px-6">{client.name}</td>
                   <td className="py-3 px-6">{client.email}</td>
                   <td className="py-3 px-6">{client.date}</td>
                   <td className="py-3 px-6 text-center">
-                    <Link to={`/ClientSurvey/${client._id}`} className="text-blue-500 hover:underline">
+                    <Link
+                      to={`/ClientSurvey/${client._id}`}
+                      className="text-blue-500 hover:underline"
+                    >
                       View
                     </Link>
                   </td>
@@ -104,7 +132,9 @@ const DashboardPage = () => {
       {/* Tenant Data Table */}
       {showTenantData && !loadingTenants && tenantData.length > 0 && (
         <div className="overflow-x-auto w-full max-w-4xl bg-white rounded-lg shadow-lg p-6">
-          <h2 className="text-xl font-semibold mb-4">Tenants Survey Responses</h2>
+          <h2 className="text-xl font-semibold mb-4">
+            Tenants Survey Responses
+          </h2>
           <table className="min-w-full bg-white">
             <thead>
               <tr className="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
@@ -115,11 +145,17 @@ const DashboardPage = () => {
             </thead>
             <tbody className="text-gray-600 text-sm font-light">
               {tenantData.map((tenant) => (
-                <tr key={tenant._id} className="border-b border-gray-200 hover:bg-gray-100">
+                <tr
+                  key={tenant._id}
+                  className="border-b border-gray-200 hover:bg-gray-100"
+                >
                   <td className="py-3 px-6">{tenant.name}</td>
                   <td className="py-3 px-6">{tenant.email}</td>
                   <td className="py-3 px-6 text-center">
-                    <Link to={`/TenantsSurvey/${tenant._id}`} className="text-blue-500 hover:underline">
+                    <Link
+                      to={`/TenantsSurvey/${tenant._id}`}
+                      className="text-blue-500 hover:underline"
+                    >
                       View
                     </Link>
                   </td>
